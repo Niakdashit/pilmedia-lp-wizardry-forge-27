abccb72b4a5087ad9525beb9760830535d61d553
Niakdashit <jonanzau13@gmail.com>
2025-05-23 10:40:15 +0200

    Delete project 11 directory

--- project 11/src/components/GameTypes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Quiz } from './Quiz';
-export { default as Wheel } from './Wheel';
-export { default as Scratch } from './Scratch';
-export { default as Swiper } from './Swiper';
